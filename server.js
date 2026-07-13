13a03404afb5d473fe7cb0c84855446b4f8bf008
Ramita <ramita.pen@gmail.com>
2026-07-13 13:49:47 +0700

    fix: add CORS support for Vercel deployment

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const cors = require('cors')
 
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }));
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173', // หรือ '*' ถ้าต้องการเปิดให้หมด
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
 readdirSync('./routes')
 .map((c) => app.use('/api',
 require('./routes/' + c)))
