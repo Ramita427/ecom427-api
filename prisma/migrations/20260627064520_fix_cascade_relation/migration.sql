1918ccd53e9fc6e715cb246282cd770dcc35aae5
Ramita <ramita.pen@gmail.com>
2026-07-23 19:18:48 +0700

    fix: AppRoutes component fix and build pass

--- prisma/migrations/20260627064520_fix_cascade_relation/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `productoncart` DROP FOREIGN KEY `ProductOnCart_productId_fkey`;
-
--- DropIndex
-DROP INDEX `ProductOnCart_productId_fkey` ON `productoncart`;
-
--- AddForeignKey
-ALTER TABLE `ProductOnCart` ADD CONSTRAINT `ProductOnCart_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- routes/stripe.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { authCheck } = require("../middlewares/authCheck");
-const router = express.Router();
-const { payment } = require("../controllers/stripe");
-
-router.post("/user/create-payment-intent", authCheck, payment);
-
-module.exports = router;
